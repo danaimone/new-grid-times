3e42228f1d4c30950aeded6a70a9ea72d4e36275
Dan Aimone <30274286+danaimone@users.noreply.github.com>
2021-11-02 17:19:01 -0700

    finished exercise 5, finished workshop

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,6 +4,7 @@ import styled from 'styled-components/macro';
 import MaxWidthWrapper from '../MaxWidthWrapper';
 
 import VisuallyHidden from '../VisuallyHidden';
+import {QUERIES} from "../../constants";
 
 const Footer = () => {
   return (
@@ -144,6 +145,18 @@ const TopRow = styled.div`
   font-size: 0.875rem;
   border-bottom: 1px solid var(--color-gray-700);
   padding: 24px 0;
+  
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: row;
+    justify-content: center;
+    gap: 48px;
+  }
+  
+  @media ${QUERIES.laptopAndUp} {
+    flex-direction: row;
+    justify-content: end;
+    gap: 48px;
+  }
 `;
 
 const Social = styled.div`
@@ -170,6 +183,13 @@ const MainNavArea = styled.div`
   gap: 32px;
   padding: 32px 0 48px;
   text-align: center;
+  
+  @media ${QUERIES.tabletAndUp} {
+    display: grid;
+    flex-direction: revert;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    text-align: left;
+  }
 `;
 
 const MainNavHeading = styled.h2`
@@ -196,6 +216,11 @@ const Subfooter = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+
+  @media ${QUERIES.tabletAndUp} {
+    align-items: flex-start;
+  }
 `;
 
 const Logo = styled.a`
